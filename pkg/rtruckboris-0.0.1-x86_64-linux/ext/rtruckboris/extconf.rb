75fccb53c524b27ea9b2fbb7cfcfec357337256b
cedlemo <cedlemo@gmx.com>
2015-03-28 09:25:51 +0100

    clean build files

--- pkg/rtruckboris-0.0.1-x86_64-linux/ext/rtruckboris/extconf.rb
+++
@@ -1,6 +0,0 @@
-require 'mkmf'
-CONFIG['CXX']='clang++'
-have_library("truckboris")
-$LIBS += " " + `pkg-config --libs truckboris`.chomp
-$CPPFLAGS += " " + `pkg-config --cflags truckboris`.chomp + " -fcxx-exceptions"
-create_makefile('rtruckboris')
